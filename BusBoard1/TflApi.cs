9deea864d49df655e9c75f8667a748a2069955f2
Ladun Omideyi <Ladun.Omideyi@softwire.com>
2021-11-19 14:46:55 +0000

    Made changes

--- BusBoard1/TflApi.cs
+++ BusBoard1/TflApi.cs
@@ -1,7 +1,14 @@
-﻿namespace BusBoard1
+﻿using RestSharp;
+
+namespace BusBoard1
 {
     public class TflApi
     {
         
+        public RestClient Client = new RestClient("https://api.twitter.com/1.1");
+        
+        var request = new RestRequest("statuses/home_timeline.json", DataFormat.Json);
+        
+        var response = client.Get(request);
     }
 }
