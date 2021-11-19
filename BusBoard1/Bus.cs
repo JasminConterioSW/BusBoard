7ee180a6c5621a2b42b4137900afe15085b00926
Ladun Omideyi <Ladun.Omideyi@softwire.com>
2021-11-19 17:26:30 +0000

    WIP Trying to extract json

---
+++ BusBoard1/Bus.cs
@@ -0,0 +1,16 @@
+﻿namespace BusBoard1
+{
+    public class Bus
+    {
+        public int timeToStation { get; set; }
+        public string destinationName { get; set; }
+        public string lineName { get; set; } //linename
+        
+        public Bus(int timeToStation, string destinationName, string lineName)
+        {
+            timeToStation = timeToStation;
+            destinationName = destinationName;
+            lineName = lineName;
+        }
+    }
+}
--- BusBoard1/TflApi.cs
+++ BusBoard1/TflApi.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Net.Http.Json;
 using RestSharp;
+using System.Text.Json;
 
 namespace BusBoard1
 
@@ -9,14 +10,6 @@ namespace BusBoard1
     public class TflApi
     {
 
-        public static string GetBusTimesJsonFromTflApi(string busStopCode)
-        {
-            var Client = new RestClient("https://api.tfl.gov.uk/");
-            var request = new RestRequest($"StopPoint/{busStopCode}/Arrivals", DataFormat.Json);
-            var response = Client.Get(request);
-
-            return response.Content;
-        }
         
         public static List<string> GetBusTimes(string busStopCode, int nBusses)
         {
@@ -25,13 +18,40 @@ namespace BusBoard1
 
             string jsonResponse = GetBusTimesJsonFromTflApi(busStopCode);
             
-            Console.WriteLine(jsonResponse);
+            //Console.WriteLine(jsonResponse);
+
+            DeserializeJson(jsonResponse);
 
             return nextBusses;
         }
 
-        
-        
+        public static string GetBusTimesJsonFromTflApi(string busStopCode)
+        {
+            var Client = new RestClient("https://api.tfl.gov.uk/");
+            var request = new RestRequest($"StopPoint/{busStopCode}/Arrivals", DataFormat.Json);
+            var response = Client.Get(request);
+            
+            return response.Content;
+        }
+
+        public static void DeserializeJson(string jsonString)
+        {
+            List<Bus> newBusList = 
+                 JsonSerializer.Deserialize<List<Bus>>(jsonString);  //this bit isn't working yet
+            //Console.WriteLine(newBusList);
+
+            foreach (var bus in newBusList)
+            {
+                Console.WriteLine(bus.timeToStation);
+            }
+
+            // public Bus( int timeToStation, string destinationName, string lineName)
+            // {
+            //     TimeToStationInSeconds = timeToStation;
+            //     Destination = 
+            //     
+            // }
+        }
     }
 
     
