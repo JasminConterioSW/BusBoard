b8cc9e32b7123f11b15e7efe96d533606ef026a0
Ladun Omideyi <Ladun.Omideyi@softwire.com>
2021-11-22 16:46:53 +0000

    Add buses controller and view

---
+++ BusBoard1.Web/Controllers/ArrivalsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BusBoard1.Web.Models;
+
+namespace BusBoard1.Web.Controllers
+{
+    public class ArrivalsController : Controller
+    {
+        private readonly ILogger<ArrivalsController> _logger;
+
+        public ArrivalsController(ILogger<ArrivalsController> logger)
+        {
+            _logger = logger;
+        }
+        
+        public IActionResult Buses()
+        {
+            
+            return View();
+        }
+    }
+}
--- BusBoard1.Web/Models/BusViewModel.cs
+++ BusBoard1.Web/Models/BusViewModel.cs
@@ -1,12 +1,10 @@
-﻿namespace BusBoard1.Api.Models
+﻿namespace BusBoard1.Web.Models
 {
     public class Bus
     {
         public int TimeToStation { get; set; }
         public string DestinationName { get; set; }
         public string LineName { get; set; }
-        
         public string StationName { get; set; }
     }
 }
-.wEWeb.
